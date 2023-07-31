865dd963381fe213e8f7563c873b25d8cdf2339e
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2023-07-31 02:48:02 -0400

    Add cat into pets list

---
+++ lua/pets/pets/cat.lua
@@ -0,0 +1,23 @@
+return {
+    next_actions = {
+        crouch = { "liedown", "sneak", "sit" },
+        idle = { "idle_blink", "walk", "sit" },
+        idle_blink = { "idle", "walk", "sit" },
+        liedown = { "sneak", "crouch" },
+        sit = { "idle", "idle_blink", "crouch", "liedown" },
+        sneak = { "crouch", "walk", "liedown" },
+        walk = { "idle", "idle_blink" },
+    },
+    idle_actions = { "idle", "idle_blink", "sit", "liedown", "sneak" },
+    first_action = "idle",
+    -- movements = {
+    --     right = {
+    --         normal = { "walk" },
+    --         fast = { "run" },
+    --     },
+    --     left = {
+    --         normal = { "walk_left" },
+    --         fast = { "run_left" },
+    --     },
+    -- },
+}
