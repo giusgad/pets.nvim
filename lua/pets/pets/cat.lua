c617fcbddd622c2c42eac31ce3512d75aa9f2be8
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2023-07-31 03:03:39 -0400

    Try something with movement

--- lua/pets/pets/cat.lua
+++ lua/pets/pets/cat.lua
@@ -11,15 +11,13 @@ return {
     idle_actions = { "idle", "idle_blink", "sit", "liedown", "sneak" },
     first_action = "idle",
     movements = {
-        normal = { "walk" },
-        fast = { "run" },
-        -- right = {
-        --     normal = { "walk" },
-        --     fast = { "run" },
-        -- },
-        -- left = {
-        --     normal = { "walk_left" },
-        --     fast = { "run_left" },
-        -- },
+        right = {
+            normal = { "walk" },
+            fast = { "run" },
+        },
+        left = {
+            normal = { "walk" },
+            fast = { "run" },
+        },
     },
 }
