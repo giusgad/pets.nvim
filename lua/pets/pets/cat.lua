bedfe253020a3f4f972eb6ea14d6625d1d8b4ceb
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2023-07-31 03:23:31 -0400

    add walk_left

--- lua/pets/pets/cat.lua
+++ lua/pets/pets/cat.lua
@@ -1,14 +1,16 @@
 return {
     next_actions = {
         crouch = { "liedown", "sneak", "sit" },
-        idle = { "idle_blink", "walk", "sit" },
+        idle = { "idle_blink", "walk", "sit", "walk_left" },
         idle_blink = { "idle", "walk", "sit" },
         liedown = { "sneak", "crouch" },
         sit = { "idle", "idle_blink", "crouch", "liedown" },
         sneak = { "crouch", "walk", "liedown" },
         walk = { "idle", "idle_blink" },
+        run_left = { "run_left", "walk_left", "run" },
+        walk_left = { "walk_left", "run_left", "idle" },
     },
-    idle_actions = { "idle", "idle_blink", "sit", "liedown", "sneak" },
+    idle_actions = { "idle", "idle_blink", "sit", "liedown" },
     first_action = "idle",
     movements = {
         right = {
